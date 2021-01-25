43fa9efe7032354f82c3088863383ecff08d4014
Flacoleo <77980066+Flacoleo@users.noreply.github.com>
2021-01-25 17:45:59 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/logo_dos.png') {
+      myImage.setAttribute('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute('src','images/logo_dos.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Escriba su nombre.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('Irene', myName);
+    myHeading.innerHTML = 'Ananassi Virtual Solutions, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('Irene')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Ananassi Virtual Solutions, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
